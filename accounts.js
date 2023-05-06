15e8b78e58cac33c5c433eb6bfda21024e0a39c7
Domenez25 <126860751+Domenez25@users.noreply.github.com>
2023-05-07 00:52:23 +0100

    login session update
    now you can keep in logged in till the end of the session or till manually disconnecting, the accounts are all provided in the local storage

--- accounts.js
+++ accounts.js
@@ -4,7 +4,9 @@ const users = {
         firstName: 'sang-heyok',
         password: 'teams',
         pfp: 'example_pfp_url', 
-        admin: true
+        admin: true ,
+        bio: 'esports member since childhood',
+        email: 'skt1faker@teams.com'
         },
     
     teamCaps: {
@@ -12,7 +14,9 @@ const users = {
         firstName: 'Sundelin',
         password: 'teams',
         pfp: 'example_pfp_url', 
-        admin: true
+        admin: true,
+        bio: 'lol player',
+        email: 'g2caps@teams.com'
         },
 
     teamTalon: {
@@ -20,7 +24,69 @@ const users = {
         firstName: 'sartori',
         password: 'teams',
         pfp: 'example_pfp_url', 
-        admin: true
+        admin: true,
+        bio: 'best akali in the world',
+        email: 'psgtalon@teams.com'
+        },
+
+    domenez25: {
+        lastName: 'zakaria', 
+        firstName: 'Bouzara',
+        password: 'teams',
+        pfp: 'example_pfp_url', 
+        admin: false,
+        bio: 'I just like to watch some streams',
+        email: 'bouzara.zakaria.25@gmail.com'
+        },
+
+    evilishie: {
+        lastName: 'raid', 
+        firstName: 'baroud',
+        password: 'teams',
+        pfp: 'example_pfp_url', 
+        admin: false,
+        bio: 'a brawlhalla star',
+        email: 'raidbarouu@email.com'
+        },
+
+    skybeam: {
+        lastName: 'tarek', 
+        firstName: 'bouzara',
+        password: 'teams',
+        pfp: 'example_pfp_url', 
+        admin: false,
+        bio: 'jungler number one in euw',
+        email: 'skybeaams@hotmai.com'
+        },
+
+    bilimachivia: {
+        lastName: 'anes', 
+        firstName: 'mecif',
+        password: 'teams',
+        pfp: 'example_pfp_url', 
+        admin: false,
+        bio: 'mf my fav champ',
+        email: 'rikos@teams.com'
+        },
+
+    estarossa: {
+        lastName: 'abdellah', 
+        firstName: 'khelifa',
+        password: 'teams',
+        pfp: 'example_pfp_url', 
+        admin: false,
+        bio: 'still gold but can carry your games',
+        email: 'rozan@gmail.com'
+        },
+    
+    rikuy: {
+        lastName: 'dahmen', 
+        firstName: 'merzoug',
+        password: 'teams',
+        pfp: 'example_pfp_url', 
+        admin: false,
+        bio: 'not falling asleep yet, still paying overwatch',
+        email: 'dahouriku@patron.com'
         },
 };
 
--- scripts.js
+++ scripts.js
@@ -11,3 +11,24 @@ function openLogin() {
     window.open(url, "Login/Signup", "width=" + width + ",height=" + height + ",left=" + left + ",top=" + top);
 }
 
+  
+  function loginButton() {
+    const cookies = document.cookie.split('; ');
+    let isLoggedIn = false;
+    let username = null;
+    
+    for (let cookie of cookies) {
+        const [name, value] = cookie.split('=');
+        if ((value === 'admin' || value === 'not_admin')) {
+            isLoggedIn = true;
+            break;
+        }
+    }
+    
+    if (isLoggedIn) {
+        window.location.href = 'profile.html';
+    } else {
+      openLogin();
+    }
+}
+  
